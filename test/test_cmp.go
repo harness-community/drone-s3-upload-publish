cec08000d3edba19c7c901f1cef46894b42ef7ab
Senthil <c_senthil.kumar@harness.io>
2024-11-12 10:22:32 +0530

    cmp doublestar, findfile, findfile_relpath

--- test/test_cmp.go
+++ test/test_cmp.go
@@ -3,35 +3,12 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/bmatcuk/doublestar/v4"
 	"github.com/georgeJobs/go-antpathmatcher"
 	"os"
 	"path/filepath"
 	"time"
 )
-
-func checkMatches(baseDir, pattern string) {
-
-	matcher := antpathmatcher.NewAntPathMatcher()
-
-	err := filepath.Walk(
-		baseDir,
-		func(path string, info os.FileInfo, err error) error {
-			// fmt.Println(">> path ", path, " filter ", pattern)
-			if err != nil {
-				return err
-			}
-			// Check if path matches the pattern
-			relativePath, _ := filepath.Rel(baseDir, path)
-			if matcher.Match(pattern, relativePath) {
-				fmt.Println("vvvvMatch:", relativePath)
-			}
-			return nil
-		})
-	if err != nil {
-		fmt.Printf("Error walking the path %s: %v\n", baseDir, err)
-	}
-	fmt.Println("====================")
-}
 
 func main() {
 	checkCVS()
@@ -45,9 +22,18 @@ func checkTest(pattern string) {
 	baseDirs := []string{
 		"/tmp/test-glob",
 	}
+	args := Args{
+		TargetDir: baseDirs[0],
+		Filter:    pattern,
+		Excludes:  "",
+	}
 
-	checkMatches(baseDirs[0], pattern)
-	ApplyFilter2("/tmp/test-glob", pattern, "")
+	fmt.Println("================= double star =================")
+	doubleStar(baseDirs[0], pattern)
+	fmt.Println("================= ApplyFilter1 =================")
+	ApplyFilter0(args)
+	fmt.Println("================= ApplyFilter2 =================")
+	ApplyFilter2(baseDirs[0], pattern, "")
 }
 
 func checkCVS() {
@@ -66,12 +52,63 @@ func checkAllTestsDir() {
 	checkTest("**/test/**")
 }
 
+func doubleStar(sourceDirStr, pattern string) {
+
+	sourceDir := os.DirFS(sourceDirStr)
+	matchedFiles, err := doublestar.Glob(sourceDir, pattern)
+	if err != nil {
+		fmt.Println("matchedDirs Error: ", err.Error())
+	}
+	for _, match := range matchedFiles {
+		fmt.Println("qqqq> ", match)
+	}
+}
+
 type FileInfo struct {
 	Name         string `json:"name"`
 	Path         string `json:"path"`
 	IsDirectory  bool   `json:"isDirectory"`
 	Length       int64  `json:"length"`
 	LastModified string `json:"lastModified"`
+}
+
+type Args struct {
+	TargetDir string
+	Filter    string
+	Excludes  string
+}
+
+func ApplyFilter0(args Args) ([]FileInfo, error) {
+	var files []FileInfo
+	m := antpathmatcher.NewAntPathMatcher()
+
+	if args.TargetDir == "" {
+		args.TargetDir = "."
+	}
+
+	err := filepath.WalkDir(args.TargetDir, func(path string, d os.DirEntry, e error) error {
+
+		if m.Match(args.Filter, path) {
+			if m.Match(args.Excludes, path) {
+				fmt.Println("eeee. ", path)
+				//fmt.Printf("path %s match exclude criteria %s", path, args.Excludes)
+			} else {
+				file, err := getFileInfo(path)
+				if err != nil {
+					return errors.New("Bad") //
+				}
+
+				files = append(files, file)
+			}
+		}
+
+		return nil
+	})
+	if err != nil {
+		return []FileInfo{}, err
+	}
+
+	return files, nil
 }
 
 func ApplyFilter2(targetDir, filter, excludes string) ([]FileInfo, error) {
